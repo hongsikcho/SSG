d67442d30b0c2a5ab56d315a6e0dfd324f7bd571
hongsikcho <jjdd9758@naver.com>
2022-07-13 17:04:57 +0900

    파일 입출력 구현

--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -37,4 +37,14 @@ public class AppTest {
 
     }
 
+    @Test
+    public void 문자열을_파일에_저장() {
+        Util.file.mkdir("test_data");
+        Util.file.saveToFile("test_data/1.txt", "안녕");
+
+        String body = Util.file.readFromFile("test_data/1.txt");
+
+        assertEquals("안녕", body);
+    }
+
 }

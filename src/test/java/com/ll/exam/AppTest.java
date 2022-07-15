457ac71b3e2d5fed4b87854d4dcc75cc7bb1cea4
hongsikcho <jjdd9758@naver.com>
2022-07-15 16:53:45 +0900

    map을 객체로 변환

--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -4,11 +4,42 @@ import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.Map;
 import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.*;
 //기본 테스트
 public class AppTest {
+
+    @Test
+    void 맵을_객체로_변경() {
+        Util.mkdir("test_data");
+        WiseSaying wiseSaying = new WiseSaying(1, "내 사전에 불가능은 없다.", "나폴레옹");
+        Util.saveToFile("test_data/1.json", wiseSaying.toJson());
+
+        String rs = Util.readFromFile("test_data/1.json");
+        Map<String, Object> map = Util.jsonToMap(rs);
+
+        WiseSaying wiseSaying1 = new WiseSaying(map);
+
+        assertEquals(1, map.get("id"));
+        assertEquals("내 사전에 불가능은 없다.", map.get("content"));
+        assertEquals(wiseSaying, wiseSaying1);
+
+    }
+    @Test
+    void 파일에_있는_JSON을_맵으로_변환() {
+        Util.mkdir("test_data");
+        WiseSaying wiseSaying = new WiseSaying(1, "내 사전에 불가능은 없다.", "나폴레옹");
+        Util.saveToFile("test_data/1.json", wiseSaying.toJson());
+
+        String rs = Util.readFromFile("test_data/1.json");
+        Map<String, Object> map = Util.jsonToMap(rs);
+
+        assertEquals(1, map.get("id"));
+        assertEquals("내 사전에 불가능은 없다.", map.get("content"));
+        assertEquals("나폴레옹", map.get("author"));
+    }
 
     @Test
     void 파일에_내용쓰기() {
